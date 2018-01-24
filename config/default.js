40517556333121a4231a5a3e4243c1aadb7e553c
huoge <1102194607@qq.com>
2018-01-24 11:07:32 +0800

    'image上传'

--- config/default.js
+++ config/default.js
@@ -1,11 +1,11 @@
 
 module.exports = {
-  port: 3000,
-  session: {
-    secret: 'myblog', // port: 程序启动要监听的端口号
-    key: 'myblog',    // session: express-session 的配置信息，后面介绍
-    // maxAge: 2592000000   // session过期时间(默认)
-    maxAge: 86400000
+  port: 3000,   // port: 程序启动要监听的端口号
+  session: {    // session: express-session 的配置信息，后面介绍
+    secret: 'myblog',
+    key: 'myblog',
+    // maxAge: 2592000000
+    maxAge: 86400000    // session过期时间(默认)
   },
   mongodb: 'mongodb://localhost:27017/myblog' // mongodb: mongodb 的地址，以 mongodb:// 协议开头，myblog 为 db 名
 }
--- routes/signup.js
+++ routes/signup.js
@@ -2,21 +2,38 @@ const express = require('express')
 const router = express.Router()
 var path = require('path');
 var sha1 = require('sha1');
+var fs = require('fs');
 
 const checkNotLogin = require('../middlewares/check').checkNotLogin
 const UserModel = require('../models/users.js');
-// GET /signup 注册页
-router.get('/', checkNotLogin, function (req, res, next) {
-  res.send('注册页')
+// 先把图片存储起来
+router.post('/image', function (req, res, next) {
+  res.send({status: 'success'})
+  console.log(req.fields.avatar)
 })
-
 // POST /signup 用户注册
 router.post('/', checkNotLogin, function (req, res, next) {
-  // res.send('注册')
+  var avatar = ''
+  var imagePathName = ''
+  fs.readdir('public/image', function (err, files) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    imagePathName = files[0]
+    console.log('打印：' + `public/image/${imagePathName}`)
+    fs.readFile(`public/image/${imagePathName}`, function (err, data) {
+      if (err) {
+        console.log(err)
+      } else {
+        avatar = data
+      }
+    })
+  })
   var name = req.fields.name
   var gender = req.fields.gender
   var bio = req.fields.bio
-  // var avatar = req.fields.avatar.path.split(path.sep).pop()
+  // var avatar = req.fields.avatar.path.split(path.sep).pop()     // 不太懂
   var password = req.fields.password
   var repassword = req.fields.repassword
 
@@ -27,30 +44,31 @@ router.post('/', checkNotLogin, function (req, res, next) {
     name: name,
     gender: gender,
     bio: bio,
-    // avatar: avatar,
+    avatar: avatar,
     password: password,
     repassword: repassword
   }
   // 用户信息写入数据库
   UserModel.create(user)
     .then(function (result) {
+      res.send({status: 'success', message: avatar})
       // 此 user 是插入 mongodb 后的值，包含 _id
-      user = result.ops[0];
+      user = result.ops[0]
       // 删除密码这种敏感信息，将用户信息存入 session
       delete user.password
-      // req.session.user = user
+      req.session.user = user
       // 写入 flash
-      req.flash('success', '注册成功')
+      // req.flash('success', '注册成功')
       res.send({status: 'success', message: '注册成功'})
     })
     .catch(function (e) {
       // 注册失败，异步删除上传的头像
-      // fs.unlink(req.files.avatar.path)
+      // fs.unlink(`../public/image/${imagePathName}`)
       // 用户名被占用则跳回注册页，而不是错误页
-      // if (e.errmsg.match('duplicate key')) {
-      //   req.flash('error', '用户名已被占用')
-      // }
-      res.send({status: 'success', message: '用户名已被占用'})
+      if (e.errmsg.match('duplicate key')) {
+        // req.flash('error', '用户名已被占用')
+        res.send({status: 'error', message: '用户名已被占用'})
+      }
       // next(e)
     })
 })
